060b9b46cd8007c1a3294c505fe40d14d80c6211
To-om <toom@thehive-project.org>
2018-01-04 15:58:41 +0100

    Fix error handler

--- app/org/thp/cortex/models/Errors.scala
+++ app/org/thp/cortex/models/Errors.scala
@@ -3,6 +3,4 @@ package org.thp.cortex.models
 abstract class CortexError(message: String) extends Exception(message)
 
 case class JobNotFoundError(jobId: String) extends CortexError(s"Job $jobId not found")
-case class UnexpectedError(message: String) extends CortexError(message)
 case class AnalyzerNotFoundError(analyzerId: String) extends CortexError(s"Analyzer $analyzerId not found")
-case class InvalidRequestError(message: String) extends CortexError(message)
--- app/org/thp/cortex/services/ErrorHandler.scala
+++ app/org/thp/cortex/services/ErrorHandler.scala
@@ -1,11 +1,19 @@
 package org.thp.cortex.services
 
-import org.thp.cortex.models.{ AnalyzerNotFoundError, InvalidRequestError, JobNotFoundError, UnexpectedError }
+import org.thp.cortex.models.{ AnalyzerNotFoundError, JobNotFoundError }
 import play.api.Logger
-import play.api.http.HttpErrorHandler
-import play.api.mvc.{ RequestHeader, Result, Results }
-
+import play.api.http.{ HttpErrorHandler, Status, Writeable }
+import play.api.mvc.{ RequestHeader, ResponseHeader, Result, Results }
 import scala.concurrent.Future
+
+import play.api.libs.json.{ JsNull, JsValue, Json }
+
+import org.elasticsearch.client.transport.NoNodeAvailableException
+
+import org.elastic4play._
+import org.elastic4play.JsonFormat._
+import org.elasticsearch.index.IndexNotFoundException
+import org.elasticsearch.index.query.QueryShardException
 
 class ErrorHandler extends HttpErrorHandler {
   private[ErrorHandler] lazy val logger = Logger(getClass)
@@ -13,15 +21,40 @@ class ErrorHandler extends HttpErrorHandler {
     Results.Status(statusCode)(s"A client error occurred on ${request.method} ${request.uri} : $message")
   }
 
-  def onServerError(request: RequestHeader, exception: Throwable): Future[Result] = {
-    val result = exception match {
-      case JobNotFoundError(jobId)           ⇒ Results.NotFound(s"Job $jobId not found")
-      case UnexpectedError(message)          ⇒ Results.InternalServerError(message)
-      case AnalyzerNotFoundError(analyzerId) ⇒ Results.NotFound(s"analyzer $analyzerId not found")
-      case InvalidRequestError(message)      ⇒ Results.BadRequest(message)
-      case error                             ⇒ Results.InternalServerError(s"Unexpected error: $error")
+  def toErrorResult(ex: Throwable): Option[(Int, JsValue)] = {
+    ex match {
+      case AuthenticationError(message)        ⇒ Some(Status.UNAUTHORIZED → Json.obj("type" → "AuthenticationError", "message" → message))
+      case AuthorizationError(message)         ⇒ Some(Status.FORBIDDEN → Json.obj("type" → "AuthorizationError", "message" → message))
+      case UpdateError(_, message, attributes) ⇒ Some(Status.INTERNAL_SERVER_ERROR → Json.obj("type" → "UpdateError", "message" → message, "object" → attributes))
+      case InternalError(message)              ⇒ Some(Status.INTERNAL_SERVER_ERROR → Json.obj("type" → "InternalError", "message" → message))
+      case nfe: NumberFormatException          ⇒ Some(Status.BAD_REQUEST → Json.obj("type" → "NumberFormatException", "message" → ("Invalid format " + nfe.getMessage)))
+      case NotFoundError(message)              ⇒ Some(Status.NOT_FOUND → Json.obj("type" → "NotFoundError", "message" → message))
+      case BadRequestError(message)            ⇒ Some(Status.BAD_REQUEST → Json.obj("type" → "BadRequest", "message" → message))
+      case SearchError(message, cause)         ⇒ Some(Status.BAD_REQUEST → Json.obj("type" → "SearchError", "message" → s"$message (${cause.getMessage})"))
+      case ace: AttributeCheckingError         ⇒ Some(Status.BAD_REQUEST → Json.toJson(ace))
+      case iae: IllegalArgumentException       ⇒ Some(Status.BAD_REQUEST → Json.obj("type" → "IllegalArgument", "message" → iae.getMessage))
+      case _: NoNodeAvailableException         ⇒ Some(Status.INTERNAL_SERVER_ERROR → Json.obj("type" → "NoNodeAvailable", "message" → "ElasticSearch cluster is unreachable"))
+      case CreateError(_, message, attributes) ⇒ Some(Status.INTERNAL_SERVER_ERROR → Json.obj("type" → "CreateError", "message" → message, "object" → attributes))
+      case ErrorWithObject(tpe, message, obj)  ⇒ Some(Status.BAD_REQUEST → Json.obj("type" → tpe, "message" → message, "object" → obj))
+      case GetError(message)                   ⇒ Some(Status.INTERNAL_SERVER_ERROR → Json.obj("type" → "GetError", "message" → message))
+      case MultiError(message, exceptions) ⇒
+        val suberrors = exceptions.map(e ⇒ toErrorResult(e)).collect {
+          case Some((_, j)) ⇒ j
+        }
+        Some(Status.MULTI_STATUS → Json.obj("type" → "MultiError", "error" → message, "suberrors" → suberrors))
+      case JobNotFoundError(jobId)           ⇒ Some(Status.NOT_FOUND -> Json.obj("type" -> "JobNotFoundError", "message" -> s"Job $jobId not found"))
+      case AnalyzerNotFoundError(analyzerId) ⇒ Some(Status.NOT_FOUND -> Json.obj("type" -> "AnalyzerNotFoundError", "message" -> s"analyzer $analyzerId not found"))
+      case _: IndexNotFoundException ⇒ Some(520 → JsNull)
+      case qse: QueryShardException  ⇒ Some(Status.BAD_REQUEST → Json.obj("type" → "Invalid search query", "message" → qse.getMessage))
+      case t: Throwable              ⇒ Option(t.getCause).flatMap(toErrorResult)
     }
-    Logger.info(s"${request.method} ${request.uri} returned ${result.header.status}", exception)
-    Future.successful(result)
+  }
+
+  def toResult[C](status: Int, c: C)(implicit writeable: Writeable[C]) = Result(header = ResponseHeader(status), body = writeable.toEntity(c))
+
+  def onServerError(request: RequestHeader, exception: Throwable): Future[Result] = {
+    val (status, body) = toErrorResult(exception).getOrElse(Status.INTERNAL_SERVER_ERROR → Json.obj("type" → exception.getClass.getName, "message" → exception.getMessage))
+    logger.info(s"${request.method} ${request.uri} returned $status", exception)
+    Future.successful(toResult(status, body))
   }
 }
