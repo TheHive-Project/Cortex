56a79f08a714454866c9d412dcb8fdfde90cd046
Nabil Adouani <nabil.adouani@gmail.com>
2018-01-19 16:52:24 +0100

    Remove analyzer cache

--- www/src/app/core/services/AnalyzerService.js
+++ www/src/app/core/services/AnalyzerService.js
@@ -2,113 +2,118 @@
 
 import _ from 'lodash';
 
-export default function(app) {
-  app.service('AnalyzerService', function($q, $http) {
+class AnalyzerService {
+  constructor($q, $http) {
+    'ngInject';
+
+    this.$q = $q;
+    this.$http = $http;
+
     this.analyzerDefinitions = null;
     this.analyzers = null;
     this.dataTypes = {};
+  }
 
-    this.getTypes = function() {
-      return this.dataTypes;
-    };
+  getTypes() {
+    return this.dataTypes;
+  }
 
-    this.definitions = function() {
-      let defered = $q.defer();
+  definitions() {
+    let defered = this.$q.defer();
 
-      if (this.analyzerDefinitions === null) {
-        $http.get('./api/analyzerdefinition').then(
-          response => {
-            this.analyzerDefinitions = _.keyBy(response.data, 'id');
+    if (this.analyzerDefinitions === null) {
+      this.$http.get('./api/analyzerdefinition').then(
+        response => {
+          this.analyzerDefinitions = _.keyBy(response.data, 'id');
 
-            defered.resolve(this.analyzerDefinitions);
-          },
-          response => {
-            defered.reject(response);
-          }
+          defered.resolve(this.analyzerDefinitions);
+        },
+        response => {
+          defered.reject(response);
+        }
+      );
+    } else {
+      defered.resolve(this.analyzerDefinitions);
+    }
+
+    return defered.promise;
+  }
+
+  list() {
+    let defered = this.$q.defer();
+
+    this.$http.get('./api/analyzer').then(
+      response => {
+        this.analyzers = response.data;
+
+        this.dataTypes = _.mapValues(
+          _.groupBy(_.flatten(_.map(response.data, 'dataTypeList'))),
+          value => value.length
         );
-      } else {
-        defered.resolve(this.analyzerDefinitions);
+
+        defered.resolve(response.data);
+      },
+      response => {
+        defered.reject(response);
       }
+    );
 
-      return defered.promise;
-    };
+    return defered.promise;
+  }
 
-    this.list = function() {
-      let defered = $q.defer();
+  run(id, artifact) {
+    let postData;
 
-      if (this.analyzers === null) {
-        $http.get('./api/analyzer').then(
-          response => {
-            this.analyzers = response.data;
+    if (artifact.dataType === 'file') {
+      postData = {
+        data: artifact.attachment,
+        dataType: artifact.dataType,
+        tlp: artifact.tlp.value
+      };
 
-            this.dataTypes = _.mapValues(
-              _.groupBy(_.flatten(_.map(response.data, 'dataTypeList'))),
-              value => value.length
-            );
+      return this.$http({
+        method: 'POST',
+        url: './api/analyzer/' + id + '/run',
+        headers: {
+          'Content-Type': undefined
+        },
+        transformRequest: data => {
+          let formData = new FormData(),
+            copy = angular.copy(data, {}),
+            _json = {};
 
-            defered.resolve(response.data);
-          },
-          response => {
-            defered.reject(response);
-          }
-        );
-      } else {
-        defered.resolve(this.analyzers);
-      }
+          angular.forEach(data, (value, key) => {
+            if (
+              Object.getPrototypeOf(value) instanceof Blob ||
+              Object.getPrototypeOf(value) instanceof File
+            ) {
+              formData.append(key, value);
+              delete copy[key];
+            } else {
+              _json[key] = value;
+            }
+          });
 
-      return defered.promise;
-    };
+          formData.append('_json', angular.toJson(_json));
 
-    this.run = function(id, artifact) {
-      let postData;
-
-      if (artifact.dataType === 'file') {
-        postData = {
-          data: artifact.attachment,
+          return formData;
+        },
+        data: postData
+      });
+    } else {
+      postData = {
+        data: artifact.data,
+        attributes: {
           dataType: artifact.dataType,
           tlp: artifact.tlp.value
-        };
+        }
+      };
 
-        return $http({
-          method: 'POST',
-          url: './api/analyzer/' + id + '/run',
-          headers: {
-            'Content-Type': undefined
-          },
-          transformRequest: data => {
-            let formData = new FormData(),
-              copy = angular.copy(data, {}),
-              _json = {};
+      return this.$http.post('./api/analyzer/' + id + '/run', postData);
+    }
+  }
+}
 
-            angular.forEach(data, (value, key) => {
-              if (
-                Object.getPrototypeOf(value) instanceof Blob ||
-                Object.getPrototypeOf(value) instanceof File
-              ) {
-                formData.append(key, value);
-                delete copy[key];
-              } else {
-                _json[key] = value;
-              }
-            });
-
-            formData.append('_json', angular.toJson(_json));
-
-            return formData;
-          },
-          data: postData
-        });
-      } else {
-        postData = {
-          data: artifact.data,
-          attributes: {
-            dataType: artifact.dataType,
-            tlp: artifact.tlp.value
-          }
-        };
-
-        return $http.post('./api/analyzer/' + id + '/run', postData);
-      }
-    };
-  });
+export default function(app) {
+  app.service('AnalyzerService', AnalyzerService);
 }
