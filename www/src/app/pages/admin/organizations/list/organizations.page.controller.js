c8de21c9a250922a26741e98d79df683ccdcaa65
Nabil Adouani <nabil.adouani@gmail.com>
2018-02-09 14:34:48 +0100

    Add confirmation dialog when enabling/disabling an organization

--- www/src/app/pages/admin/organizations/list/organizations.page.controller.js
+++ www/src/app/pages/admin/organizations/list/organizations.page.controller.js
@@ -3,11 +3,13 @@
 import _ from 'lodash/core';
 
 export default class OrganizationsPageController {
-  constructor($log, OrganizationService) {
+  constructor($log, OrganizationService, NotificationService, ModalService) {
     'ngInject';
 
     this.$log = $log;
     this.OrganizationService = OrganizationService;
+    this.NotificationService = NotificationService;
+    this.ModalService = ModalService;
   }
 
   $onInit() {
@@ -28,17 +30,49 @@ export default class OrganizationsPageController {
   }
 
   disable(id) {
-    this.OrganizationService.disable(id).then(response => {
-      this.$log.log(`Organization ${id} deleted`, response);
-      this.reload();
-    });
+    let modalInstance = this.ModalService.confirm(
+      'Disable organization',
+      `Are your sure you want to disable this organization? Its users will no longer be able to have access to Cortex.`,
+      {
+        flavor: 'danger',
+        okText: 'Yes, disable it'
+      }
+    );
+
+    modalInstance.result
+      .then(() => this.OrganizationService.disable(id))
+      .then(() => {
+        this.reload();
+        this.NotificationService.success('The organization has been disabled');
+      })
+      .catch(err => {
+        if (!_.isString(err)) {
+          this.NotificationService.error('Unable to disable the organization.');
+        }
+      });
   }
 
   enable(id) {
-    this.OrganizationService.enable(id).then(response => {
-      this.$log.log(`Organization ${id} deleted`, response);
-      this.reload();
-    });
+    let modalInstance = this.ModalService.confirm(
+      'Enable organization',
+      `Are your sure you want to enable this organization? Its users will have access to Cortex.`,
+      {
+        flavor: 'primary',
+        okText: 'Yes, enable it'
+      }
+    );
+
+    modalInstance.result
+      .then(() => this.OrganizationService.enable(id))
+      .then(() => {
+        this.reload();
+        this.NotificationService.success('The organization has been enabled');
+      })
+      .catch(err => {
+        if (!_.isString(err)) {
+          this.NotificationService.error('Unable to enabled the organization.');
+        }
+      });
   }
 
   reload() {
