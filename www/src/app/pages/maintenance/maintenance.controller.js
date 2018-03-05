37ae61d09a8baefd4e89fc427d103c7b5c75e909
Nabil Adouani <nabil.adouani@gmail.com>
2018-03-05 14:45:59 +0100

    Remove analyzer roles for superadmin

--- www/src/app/pages/maintenance/maintenance.controller.js
+++ www/src/app/pages/maintenance/maintenance.controller.js
@@ -64,7 +64,7 @@ export default class MaintenanceController {
       login: angular.lowercase(this.newUser.login),
       name: this.newUser.name,
       password: this.newUser.password,
-      roles: ['read', 'analyze', 'superadmin'],
+      roles: ['read', 'superadmin'],
       organization: 'cortex'
     }).then(() => {
       this.$state.go(this.successState);
