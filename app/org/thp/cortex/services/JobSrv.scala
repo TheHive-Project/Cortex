a99b89748684f097c7c3ab4f6a74407fc29c0bed
To-om <toom@thehive-project.org>
2018-03-28 13:58:20 +0200

    Fix rate limit in search query

--- app/org/thp/cortex/services/JobSrv.scala
+++ app/org/thp/cortex/services/JobSrv.scala
@@ -289,8 +289,8 @@ class JobSrv(
     } yield {
       import org.elastic4play.services.QueryDSL._
       val now = new Date().getTime
-      logger.info(s"Checking rate limit on analyzer ${analyzer.name} from ${new Date(now - rateUnit.id.toLong * 24 * 60 * 60 * 1000)}")
-      stats(and("createdAt" ~>= (now - rateUnit.id * 1000), "analyzerId" ~= analyzer.id), Seq(selectCount)).map { stats ⇒
+      logger.info(s"Checking rate limit on analyzer ${analyzer.name()} from ${new Date(now - rateUnit.id.toLong * 24 * 60 * 60 * 1000)}")
+      stats(and("createdAt" ~>= (now - rateUnit.id.toLong * 24 * 60 * 60 * 1000), "analyzerId" ~= analyzer.id), Seq(selectCount)).map { stats ⇒
         val count = (stats \ "count").as[Long]
         logger.info(s"$count analysis found (limit is $rate)")
         count < rate
