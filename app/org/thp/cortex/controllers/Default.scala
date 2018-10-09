b20452d75aa82f8a69ab8af6eaefcc872fb59e66
To-om <toom@thehive-project.org>
2018-10-09 15:24:03 +0200

    #140 Allow Cortex to use a custom root context

---
+++ app/org/thp/cortex/controllers/Default.scala
@@ -0,0 +1,13 @@
+package org.thp.cortex.controllers
+
+import play.api.Configuration
+import play.api.mvc.{ AbstractController, Action, AnyContent, ControllerComponents }
+
+import javax.inject.{ Inject, Singleton }
+
+@Singleton
+class Default @Inject() (configuration: Configuration, components: ControllerComponents) extends AbstractController(components) {
+  def home: Action[AnyContent] = Action {
+    Redirect(configuration.get[String]("play.http.context") + "/index.html")
+  }
+}
