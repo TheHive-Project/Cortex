163e73651585cc97c29e1f27561c06a251fd8a91
To-om <toom@thehive-project.org>
2019-04-04 15:22:37 +0200

    #181 Add organization in OAuth2 user mapper

--- app/org/thp/cortex/services/mappers/GroupUserMapper.scala
+++ app/org/thp/cortex/services/mappers/GroupUserMapper.scala
@@ -16,7 +16,9 @@ class GroupUserMapper(
     nameAttrName: String,
     rolesAttrName: Option[String],
     groupAttrName: String,
+    organizationAttrName: Option[String],
     defaultRoles: Seq[String],
+    defaultOrganization: Option[String],
     groupsUrl: String,
     mappings: Map[String, Seq[String]],
     ws: WSClient,
@@ -31,7 +33,9 @@ class GroupUserMapper(
     configuration.getOptional[String]("auth.sso.attributes.name").getOrElse("username"),
     configuration.getOptional[String]("auth.sso.attributes.roles"),
     configuration.getOptional[String]("auth.sso.attributes.groups").getOrElse(""),
+    configuration.getOptional[String]("auth.sso.attributes.organization"),
     configuration.getOptional[Seq[String]]("auth.sso.defaultRoles").getOrElse(Seq()),
+    configuration.getOptional[String]("auth.sso.defaultOrganization"),
     configuration.getOptional[String]("auth.sso.groups.url").getOrElse(""),
     configuration.getOptional[Map[String, Seq[String]]]("auth.sso.groups.mappings").getOrElse(Map()),
     ws,
@@ -50,10 +54,15 @@ class GroupUserMapper(
       val fields = for {
         login ← (jsValue \ loginAttrName).validate[String]
         name ← (jsValue \ nameAttrName).validate[String]
+        organization ← organizationAttrName
+          .flatMap(o ⇒ (jsValue \ o).asOpt[String])
+          .orElse(defaultOrganization)
+          .fold[JsResult[String]](JsError())(o ⇒ JsSuccess(o))
       } yield Fields(Json.obj(
         "login" → login,
         "name" → name,
-        "roles" → roles))
+        "roles" → roles,
+        "organization" → organization))
       fields match {
         case JsSuccess(f, _) ⇒ Future.successful(f)
         case JsError(errors) ⇒ Future.failed(AuthenticationError(s"User info fails: ${errors.map(_._1).mkString}"))
