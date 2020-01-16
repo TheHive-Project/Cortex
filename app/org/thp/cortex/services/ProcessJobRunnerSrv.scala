a79ff4db5e93b0710daf7ad93281a8b67f6adea7
To-om <toom@thehive-project.org>
2020-01-16 11:11:28 +0100

    #241 Include worker output if output file is empty

--- app/org/thp/cortex/services/ProcessJobRunnerSrv.scala
+++ app/org/thp/cortex/services/ProcessJobRunnerSrv.scala
@@ -3,21 +3,17 @@ package org.thp.cortex.services
 import java.nio.charset.StandardCharsets
 import java.nio.file.{Files, Path, Paths}
 
-import scala.concurrent.duration.FiniteDuration
-import scala.concurrent.{ExecutionContext, Future}
-import scala.sys.process.{Process, ProcessLogger}
-
-import play.api.Logger
-
 import akka.actor.ActorSystem
 import javax.inject.{Inject, Singleton}
-import org.thp.cortex.models._
-
 import org.elastic4play.utils.RichFuture
-import scala.sys.process._
-import scala.util.Try
-
+import org.thp.cortex.models._
+import play.api.Logger
 import play.api.libs.json.Json
+
+import scala.concurrent.duration.FiniteDuration
+import scala.concurrent.{ExecutionContext, Future}
+import scala.sys.process.{Process, ProcessLogger, _}
+import scala.util.Try
 
 @Singleton
 class ProcessJobRunnerSrv @Inject()(implicit val system: ActorSystem) {
@@ -37,19 +33,31 @@ class ProcessJobRunnerSrv @Inject()(implicit val system: ActorSystem) {
 
   def run(jobDirectory: Path, command: String, job: Job, timeout: Option[FiniteDuration])(implicit ec: ExecutionContext): Future[Unit] = {
     val baseDirectory = Paths.get(command).getParent.getParent
+    val output        = StringBuilder.newBuilder
     logger.info(s"Execute $command in $baseDirectory, timeout is ${timeout.fold("none")(_.toString)}")
     val process = Process(Seq(command, jobDirectory.toString), baseDirectory.toFile)
-      .run(ProcessLogger(s ⇒ logger.info(s"  Job ${job.id}: $s")))
+      .run(ProcessLogger { s ⇒
+        logger.info(s"  Job ${job.id}: $s")
+        output ++= s
+      })
     val execution = Future
       .apply {
         process.exitValue()
+        ()
+      }
+      .map { _ ⇒
+        val outputFile = jobDirectory.resolve("output").resolve("output.json")
+        if (!Files.exists(outputFile) || Files.size(outputFile) == 0) {
+          val report = Json.obj("success" → false, "errorMessage" → output.toString)
+          Files.write(outputFile, report.toString.getBytes(StandardCharsets.UTF_8))
+        }
         ()
       }
       .recoverWith {
         case error ⇒
           logger.error(s"Execution of command $command failed", error)
           Future.apply {
-            val report = Json.obj("success" → false, "errorMessage" → error.getMessage)
+            val report = Json.obj("success" → false, "errorMessage" → s"${error.getMessage}\n$output")
             Files.write(jobDirectory.resolve("output").resolve("output.json"), report.toString.getBytes(StandardCharsets.UTF_8))
             ()
           }
