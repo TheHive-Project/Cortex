18c7f95beeead321054a49765ee367c87ac1f9af
Cédric Chantepie <cchantep@users.noreply.github.com>
2026-06-19 15:55:14 +0200

    test(elastic4play): Adding unit test on existing JSON format for AttributeCheckingError (no implementation change on itself)

--- elastic4play/test/org/elastic4play/JsonFormatSpec.scala
+++ elastic4play/test/org/elastic4play/JsonFormatSpec.scala
@@ -8,52 +8,91 @@ final class JsonFormatSpec extends org.specs2.mutable.Specification {
   "Play JSON format for Elastic".title
 
   "Format for attribute errors" should {
+    val invalidFormatAttrErr = InvalidFormatAttributeError(
+      name = "Test 1",
+      format = "Foo",
+      value = JsonInputValue(JsString("Input")))
+
+    val invalidFormatAttrErrJson = Json.obj(
+      "name" -> "Test 1",
+      "format" -> "Foo",
+      "value" -> Json.obj("type" -> "JsonInputValue", "value" -> "Input"),
+      "type" -> "InvalidFormatAttributeError",
+      "message" -> "Invalid format for Test 1: JsonInputValue(\"Input\"), expected Foo")
+
     "support InvalidFormatAttributeError" in {
       implicit def w: OWrites[InvalidFormatAttributeError] =
         JsonFormat.invalidFormatAttributeErrorWrites
 
-      Json.toJsObject(InvalidFormatAttributeError(
-        name = "Test 1",
-        format = "Foo",
-        value = JsonInputValue(JsString("Input")))) must_=== Json.obj(
-          "name" -> "Test 1",
-          "format" -> "Foo",
-          "value" -> Json.obj("type" -> "JsonInputValue", "value" -> "Input"),
-          "type" -> "InvalidFormatAttributeError",
-          "message" -> "Invalid format for Test 1: JsonInputValue(\"Input\"), expected Foo")
+      Json.toJsObject(invalidFormatAttrErr) must_=== invalidFormatAttrErrJson
     }
+
+    val unknownAttrErr = UnknownAttributeError(
+      name = "Test 2",
+      value = JsString("Bar"))
+
+    val unknownAttrErrJson = Json.obj(
+      "name" -> "Test 2",
+      "value" -> "Bar",
+      "type" -> "UnknownAttributeError",
+      "message" -> "Unknown attribute Test 2: \"Bar\"")
 
     "support UnknownAttributeError" in {
       implicit def w: OWrites[UnknownAttributeError] =
         JsonFormat.unknownAttributeErrorWrites
 
-      Json.toJsObject(UnknownAttributeError(
-        name = "Test 2",
-        value = JsString("Bar"))) must_=== Json.obj(
-          "name" -> "Test 2",
-          "value" -> "Bar",
-          "type" -> "UnknownAttributeError",
-          "message" -> "Unknown attribute Test 2: \"Bar\"")
+      Json.toJsObject(unknownAttrErr) must_=== unknownAttrErrJson
     }
+
+    val updateRoAttrErr = UpdateReadOnlyAttributeError("Lorem")
+
+    val updateRoAttrErrJson = Json.obj(
+      "name" -> "Lorem",
+      "type" -> "UpdateReadOnlyAttributeError",
+      "message" -> "Attribute Lorem is read-only")
 
     "support UpdateReadOnlyAttributeError" in {
       implicit def w: OWrites[UpdateReadOnlyAttributeError] =
         JsonFormat.updateReadOnlyAttributeErrorWrites
 
-      Json.toJsObject(UpdateReadOnlyAttributeError("Lorem")) must_=== Json.obj(
-        "name" -> "Lorem",
-        "type" -> "UpdateReadOnlyAttributeError",
-        "message" -> "Attribute Lorem is read-only")
+      Json.toJsObject(updateRoAttrErr) must_=== updateRoAttrErrJson
     }
+
+    val missingAttrErr = MissingAttributeError("Ipsum")
+
+    val missingAttrErrJson = Json.obj(
+      "name" -> "Ipsum",
+      "type" -> "MissingAttributeError",
+      "message" -> "Attribute Ipsum is missing")
 
     "support MissingAttributeError" in {
       implicit def w: OWrites[MissingAttributeError] =
         JsonFormat.missingAttributeErrorWrites
 
-      Json.toJsObject(MissingAttributeError("Ipsum")) must_=== Json.obj(
-        "name" -> "Ipsum",
-        "type" -> "MissingAttributeError",
-        "message" -> "Attribute Ipsum is missing")
+      Json.toJsObject(missingAttrErr) must_=== missingAttrErrJson
+    }
+
+    "support AttributeCheckingError" in {
+      import JsonFormat.attributeCheckingExceptionWrites
+
+      val errors = Seq(invalidFormatAttrErr, unknownAttrErr, updateRoAttrErr, missingAttrErr)
+
+      val errorsJson = Seq(invalidFormatAttrErrJson, unknownAttrErrJson, updateRoAttrErrJson, missingAttrErrJson)
+
+      Json.toJsObject(AttributeCheckingError(
+        tableName = "Table 1",
+        errors = errors)) must_=== Json.obj(
+          "tableName" -> "Table 1",
+          "type" -> "AttributeCheckingError",
+          "errors" -> errorsJson) and {
+        // Reverse errors to make sure the order is preserved
+        Json.toJsObject(AttributeCheckingError(
+          tableName = "Table 2",
+          errors = errors.reverse)) must_=== Json.obj(
+          "tableName" -> "Table 2",
+            "type" -> "AttributeCheckingError",
+            "errors" -> errorsJson.reverse)
+      }
     }
   }
 }
